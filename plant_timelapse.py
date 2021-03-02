e2c8f310a77c5ee89fbc2813b32d281f08b0bae4
pardojer23 <mezeg39@gmail.com>
2021-03-02 10:57:39 -0500

    added pi camera rotation capability

--- plant_timelapse.py
+++ plant_timelapse.py
@@ -9,14 +9,16 @@ from time import sleep
 import argparse
 
 
-def timelapse(int, n, dir):
+def timelapse(int, n, dir, rotation):
     """
     Uses the PI camera to take a set of images at a user defined interval
     :param int: length of interval between images in seconds
     :param n: number of images to take
     :param dir: path to output directory where images are saved
+    :param rotation degrees to rotate pi image
     """
     camera = PiCamera()
+    camera.rotation = rotation
     for i in range(n):
         camera.capture(dir+os.pathsep()+"image{0:04d.jpg".format(i))
         sleep(int)
@@ -33,10 +35,13 @@ def main():
     parser.add_argument("--images", "-n", help="number of images", default=1)
     parser.add_argument("--output", "-o",
                         help="path to output directory", default="./")
+    parser.add_argument("--rotation", "-r",
+                        help="degrees to rotate pi image", default=0)
     args = parser.parse_args()
     interval = args.interval
     images = args.images
     output = args.output
+    rotation = args.rotation
     timelapse(int=interval, n=images, dir=output)
 
 
